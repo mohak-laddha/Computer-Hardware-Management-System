719660d77cf7746a9d30b4317581a0f8cd1daf8e
Mohak Laddha <69622244+mohak-laddha@users.noreply.github.com>
2020-11-20 02:12:49 +0530

    Q9 Shop1.java
    //Q9 Implementing Exception Handling

---
+++ Q9/myPackage1/Login.java
@@ -0,0 +1,53 @@
+package myPackage1;
+
+import java.util.*;
+
+class CustomException extends Exception		//userdefined exception
+{	 
+	public CustomException(String message)
+	{
+		super(message);		// call the constructor of parent Exception
+	    
+	  }
+}
+
+public class Login		//Class that uses above CustomException 
+{	
+	public void login()		//function login
+	{
+		Scanner sc = new Scanner(System.in);
+
+		try
+		{
+		
+		String pass;
+		String user;
+
+		System.out.println("Enter user name: ");
+		user=sc.next();
+		System.out.println("Enter password: ");
+		pass =sc.next();
+
+		if(user.equals("root") && pass.equals("toor"))
+		{
+ 			System.out.println("Login Successfully ");
+		}
+		else
+		{
+			throw new CustomException("Wrong username and password!");		// Throw an object of user defined exception 
+			
+		}
+		}
+		catch (CustomException e)
+		{
+			System.out.println("" + e + "Exception");		// Print the message from CustomException object
+			System.out.println("Enter 0 to exit and 1 to continue: ");
+			int ex = sc.nextInt();
+			if (ex == 0)
+				System.exit(0);
+			else
+				login();
+		}
+	
+	}
+}  
