de3ad8ab7a0511d93620bb3d778c76a9ef65e216
Mohak Laddha <69622244+mohak-laddha@users.noreply.github.com>
2020-12-17 10:31:03 +0530

    Add files via upload

---
+++ Computer_Hardware_Management_System/src/connector/dp1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package connector;
+
+/**
+ *
+ * @author Mohak
+ */
+class dp1 {
+
+    static void add(Open op) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
